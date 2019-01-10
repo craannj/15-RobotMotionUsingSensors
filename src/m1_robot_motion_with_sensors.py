eb3e60b5965ea7831ae5aa732282af5560fbc510
craannj <craannj@rose-hulman.edu>
2019-01-10 17:01:21 -0500

    Done

--- src/m1_robot_motion_with_sensors.py
+++ src/m1_robot_motion_with_sensors.py
@@ -4,8 +4,8 @@ wait-until-event pattern, in the context of robot motion that uses sensors.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Nathaniel Craan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -14,8 +14,8 @@ import math
 
 def main():
     """ Calls the other functions to test/demo them. """
-    run_test_wait_for_seconds()
-    run_test_init()
+    #run_test_wait_for_seconds()
+    #run_test_init()
     run_test_go_and_stop()
     run_test_go_straight_for_seconds()
     run_test_go_straight_for_inches()
@@ -38,7 +38,7 @@ def run_test_wait_for_seconds():
 def wait_for_seconds():
     """ Prints Hello, waits for 3 seconds, then prints Goodbye. """
     # -------------------------------------------------------------------------
-    # TODO: 2. With your instructor, implement and test this function.
+    # DONE: 2. With your instructor, implement and test this function.
     #   IMPORTANT:  Do NOT use the    time.sleep   function
     #               anywhere in this project.
     #               (Exception: Use it in test-functions to separate tests.)
@@ -51,6 +51,13 @@ def wait_for_seconds():
     #   NOTE: this function has nothing to do with robots,
     #   but its concepts will be useful in the forthcoming robot exercises.
     # -------------------------------------------------------------------------
+    print('Hello')
+    start = time.time()
+    while True:
+        current = time.time()
+        if current - start >= 3:
+            break
+    print("Goodbye")
 
 
 def run_test_init():
@@ -63,6 +70,7 @@ def run_test_init():
     # TODO: 3. Implement this function, then implement the   __init__   method
     #   of the SimpleRoseBot class, then use this function to test __init__.
     # -------------------------------------------------------------------------
+    SimpleRoseBot()
 
 
 def run_test_go_and_stop():
@@ -76,6 +84,14 @@ def run_test_go_and_stop():
     #   methods of the SimpleRoseBot class, then use this function
     #   to test both   go   and   stop   at the same time.
     # -------------------------------------------------------------------------
+    robot = SimpleRoseBot()
+    robot.go(50, 50)
+    start = time.time()
+    while True:
+        current = time.time()
+        if current - start >= 2:
+            break
+    robot.stop()
 
 
 def run_test_go_straight_for_seconds():
@@ -89,6 +105,9 @@ def run_test_go_straight_for_seconds():
     #   go_straight_for_seconds   method of the SimpleRoseBot class,
     #   then use this function to test that method.
     # -------------------------------------------------------------------------
+    robot = SimpleRoseBot()
+    robot.go_straight_for_seconds(7, 100)
+    robot.go_straight_for_seconds(5, -40)
 
 
 def run_test_go_straight_for_inches():
@@ -121,6 +140,32 @@ def run_test_go_straight_until_black():
 # Put your   SimpleRoseBot    class here (below this comment).
 # Your instructor may help you get started.
 ###############################################################################
+class SimpleRoseBot(object):
+
+    def __init__(self):
+        self.left_motor = Motor('B')
+        self.right_motor = Motor('C')
+        self.color_sensor = ColorSensor('3')
+
+    def go(self, left_wheel_speed, right_wheel_speed):
+        self.left_motor.turn_on(left_wheel_speed)
+        self.right_motor.turn_on(right_wheel_speed)
+
+    def stop(self):
+        self.left_motor.turn_off()
+        self.right_motor.turn_off()
+
+    def go_straight_for_seconds(self, seconds, speed):
+        self.left_motor.turn_on(speed)
+        self.right_motor.turn_on(speed)
+        start = time.time()
+        while True:
+            current = time.time()
+            if current - start >= seconds:
+                break
+        self.left_motor.turn_off()
+        self.right_motor.turn_off()
+
 
 
 ###############################################################################
